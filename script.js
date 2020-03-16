931ec87c3a446fe95039dcd2d03633e517b09b05
Yauheni Ramankou <iromankov2010@gmail.com>
2020-03-16 18:01:30 +0300

    feat(script): implement anchor prevent default

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+const ANCHOR = document.getElementsByTagName("a"),
+  ANCHOR_LENGTH = ANCHOR.length,
+  SOCIALS_ITEM = document.querySelector(".socials__item");
+
+window.prevAction = false;
+
 function prevDefault(event) {
-  event.prevDefault();
+  if (window.prevAction) {
+    event.preventDefault();
+  }
 }
+
+(function anchorPrevDefault() {
+  for (let i = 0; i < ANCHOR_LENGTH; i++) {
+    ANCHOR[i].addEventListener("click", prevDefault, false);
+  }
+})()
+
+SOCIALS_ITEM.addEventListener("click", () => window.prevAction = true, false);
