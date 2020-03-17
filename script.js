66506959c6c4016888bd3597f175c03cabd1892b
Yauheni Ramankou <iromankov2010@gmail.com>
2020-03-17 20:32:02 +0300

    feat(script): add to-top js

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function prevDefault(event) {
 SOCIALS_ITEM.addEventListener("click", () => window.location = this.href="#", false);
 
 function ready(){
+  const TO_TOP = document.querySelector(".to-top");
+
   function showHeader() {
     const HEADER = document.querySelector(".header");
     if (window.pageYOffset > 95) {
@@ -24,7 +26,22 @@ function ready(){
     }
   }
 
+  function showToTop() {
+    if (window.pageYOffset > 350) {
+      TO_TOP.classList.add("to-top--show");
+    } else {
+      TO_TOP.classList.remove("to-top--show");
+    }
+  }
+
+  function toTopScroll(){
+    console.log("It's woks");
+    window.scrollTo(0, 0);
+  }
+
   document.addEventListener("scroll", showHeader, false);
+  document.addEventListener("scroll", showToTop, false);
+  TO_TOP.addEventListener("click", toTopScroll, false);
 }
 
 document.addEventListener("DOMContentLoaded", ready);
